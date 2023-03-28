954df27420278dcc74ecaac42bd220f1b640c59b
BamiseOmolaso <davidbams3@gmail.com>
2023-03-28 09:49:16 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -2,9 +2,9 @@
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * @list: List of arguments to be printed
  * @ind: ind.
- * @buffer: Buffer array to handle print.
+ * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
  * @width: get width.
  * @precision: Precision specification
